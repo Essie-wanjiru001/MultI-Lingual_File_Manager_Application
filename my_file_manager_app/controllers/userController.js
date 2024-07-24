dc96c98e485a3f9512fd77a141bb45a98881f022
Vulgence <e.kinyua@alustudent.com>
2024-07-24 19:02:34 +0300

    routing

--- my_file_manager_app/controllers/userController.js
+++ my_file_manager_app/controllers/userController.js
@@ -5,7 +5,7 @@ exports.register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
     const user = await User.create({ username, email, password });
-    res.status(201).json({ message: 'User registered successfully', userId: user.id });
+    // res.status(201).json({ message: 'User registered successfully', userId: user.id });
     res.redirect('/login');
   } catch (error) {
     res.status(500).json({ message: 'Error registering user', error: error.message });
