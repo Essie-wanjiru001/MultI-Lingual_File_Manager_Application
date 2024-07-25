d3660cca031b15e6149f344b3c565edba1f675db
Vulgence <e.kinyua@alustudent.com>
2024-07-25 14:53:50 +0300

    i18next init

--- my_file_manager_app/app.js
+++ my_file_manager_app/app.js
@@ -12,12 +12,6 @@ const i18next = require('i18next');
 const Backend = require('i18next-fs-backend');
 const middleware = require('i18next-http-middleware');
 
-// const i18next = require('./i18nConfig');
-// const i18nextFsBackend = require('i18next-fs-backend');
-// const i18nextHttpMiddleware = require('i18next-http-middleware');
-// const path = require('path');
-// const setLanguage = require('./middleware/languageMiddleware');
-
 i18next.use(Backend).use(middleware.LanguageDetector)
   .init({
     fallbackLng: 'en',
